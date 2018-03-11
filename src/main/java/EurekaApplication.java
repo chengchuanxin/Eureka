3c6dbfb30a0984e16b5261c05ac17b8fcdd2fb98
chengchuanxin <chengchuanxin94@163.com>
2018-03-11 22:29:15 +0800

    feat: init project

---
+++ src/main/java/EurekaApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chengchuanxin chengchuanxin94@163.com
+ * @since 18-3-11
+ */
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
